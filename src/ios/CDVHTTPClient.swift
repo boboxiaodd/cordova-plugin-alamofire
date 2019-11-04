d88c11bfec0d8aa6061cf7be6c4b7b7ef6d90785
bobo <iam386@gmail.com>
2019-11-04 16:19:32 +0800

    first commit

--- src/ios/CDVHTTPClient.swift
+++ src/ios/CDVHTTPClient.swift
@@ -1,97 +1,103 @@
 import Foundation
-import YPImagePicker
-import AVFoundation
-import UIKit
+import Alamofire
 /*
 * Notes: The @objc shows that this class & function should be exposed to Cordova.
 */
+
 @objc(CDVHTTPClient) class CDVHTTPClient : CDVPlugin {
-    @objc(open:) // Declare your function name.
-    func open(command: CDVInvokedUrlCommand) { // write the function code.
-        var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "picker type error")
-        var config = YPImagePickerConfiguration()
-        config.library.onlySquare = false
-        let arg = command.argument(at: 0) as! [AnyHashable : Any]
-        switch(arg["type"] as! String ){
-            case "video":
-                config.startOnScreen = YPPickerScreen.library
-                config.library.mediaType = .video
-                config.screens = [.library, .photo]
-                config.video.fileType = .mp4
-                config.showsCrop = .none
-                config.showsVideoTrimmer = false
-                config.isScrollToChangeModesEnabled = false
-                let picker = YPImagePicker(configuration: config)
-                picker.didFinishPicking { [unowned picker] items, _ in
-                    if let video = items.singleVideo {
-                        let videoInfo = try! video.url.resourceValues(forKeys: [.fileSizeKey])
-                        let json = ["url": video.url.absoluteString,
-                                    "size": videoInfo.fileSize ?? 0,
-                                    "duration":video.asset?.duration.description ?? "0"] as [AnyHashable : Any]
-                        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: json)
-                        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
-                    }
-                    picker.dismiss(animated: true, completion: nil)
-                }
-                self.viewController.present(picker, animated: true)
-                break;
-            case "avatar":
-                config.startOnScreen = YPPickerScreen.library
-                config.library.mediaType = .photo
-                config.screens = [.library, .photo]
-                config.showsCrop = .rectangle(ratio: 1)
-                config.showsPhotoFilters = arg["filter"] as? Bool ?? false
-                config.targetImageSize = .cappedTo(size: CGFloat(arg["size"] as? Int ?? 500))
-                config.isScrollToChangeModesEnabled = false
-                config.shouldSaveNewPicturesToAlbum = false
-                let picker = YPImagePicker(configuration: config)
-                picker.didFinishPicking { [unowned picker] items, _ in
-                    if let photo = items.singlePhoto {
-                        let fileInfo = self.saveImage(image: photo.image)
-                        let json = ["url": fileInfo["path"] ?? "",
-                                    "size": fileInfo["fileSize"] ?? 0
-                                   ] as [AnyHashable : Any]
-                        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: json)
-                        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
-                    }
-                    picker.dismiss(animated: true, completion: nil)
-                }
-                self.viewController.present(picker, animated: true)
-                break;
-            case "photo":
-                config.startOnScreen = YPPickerScreen.library
-                config.library.mediaType = .photo
-                config.screens = [.library, .photo]
-                config.showsCrop = .none
-                config.targetImageSize = .cappedTo(size: CGFloat(arg["size"] as? Int ?? 1024))
-                config.isScrollToChangeModesEnabled = false
-                config.showsPhotoFilters = arg["filter"] as? Bool ?? false
-                config.shouldSaveNewPicturesToAlbum = false
-                let picker = YPImagePicker(configuration: config)
-                picker.didFinishPicking { [unowned picker] items, _ in
-                    if let photo = items.singlePhoto {
-                        let fileInfo = self.saveImage(image: photo.image)
-                        let json = ["url": fileInfo["path"] ?? "",
-                                    "size": fileInfo["size"] ?? 0,
-                                    "width": photo.image.size.width,
-                                    "height": photo.image.size.height
-                                   ] as [AnyHashable : Any]
-                        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: json)
-                        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
-                    }
-                    picker.dismiss(animated: true, completion: nil)
-                }
-                self.viewController.present(picker, animated: true)
-                break;
-            default:
+    private var filecache:RemoteFileCacheManager!
+    override func pluginInitialize() {
+        filecache = RemoteFileCacheManager(subFolder: "cache")
+    }
+
+    @objc(request:) // Declare your function name.
+    func request(command: CDVInvokedUrlCommand) { // write the function code.
+        let arg = command.arguments[0] as! [String:Any]
+        let url = arg["url"] as! String
+        let param = arg["param"] as? [String:Any]
+        let headers = arg["headers"] as? [String:String]
+        Alamofire.request(url, method: .post, parameters: param, headers: headers).responseJSON{res in
+            if res.result.isFailure {
+                let pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: res.result.debugDescription )
+                pluginResult?.setKeepCallbackAs(true)
                 self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+            }else{
+                let json = res.result.value as! [AnyHashable:Any]
+                let pluginResult = CDVPluginResult (status: CDVCommandStatus_OK, messageAs: json)
+                pluginResult?.setKeepCallbackAs(true)
+                self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+            }
         }
     }
-    func saveImage(image:UIImage) -> [AnyHashable : Any] {
-        let tmpDirectory = FileManager.default.temporaryDirectory;
-        let path = tmpDirectory.appendingPathComponent("\(UUID().uuidString).jpg")
-        try! UIImageJPEGRepresentation(image,0.8)?.write(to: path)
-        let fileInfo = try! path.resourceValues(forKeys: [.fileSizeKey])
-        return ["path":path.absoluteString,"size":fileInfo.fileSize ?? 0]
+
+    @objc(upload:)
+    func upload(command: CDVInvokedUrlCommand){
+        let arg = command.arguments[0] as! [String:Any]
+        let url = arg["url"] as! String
+        let path = arg["path"] as! String
+        let headers = arg["headers"] as? [String:String]
+
+        Alamofire.upload(multipartFormData: {multipartFormData in
+            multipartFormData.append(URL(fileURLWithPath: path), withName: "file")
+        }, to: url, method: .post, headers: headers){result in
+            switch result {
+               case .success(let upload, _, _):
+                   upload.uploadProgress(closure: { (progress) in
+                        let json = ["progress": progress] as [AnyHashable:Any]
+                        let pluginResult = CDVPluginResult (status: CDVCommandStatus_OK, messageAs: json)
+                        pluginResult?.setKeepCallbackAs(true)
+                        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+                   })
+                   upload.responseJSON { response in
+                       //print response.result
+                        let json = response.result.value as! [AnyHashable:Any]
+                        let pluginResult = CDVPluginResult (status: CDVCommandStatus_OK, messageAs: json)
+                        pluginResult?.setKeepCallbackAs(true)
+                        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+                   }
+               case .failure(let encodingError):
+                   let pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: encodingError.localizedDescription)
+                   pluginResult?.setKeepCallbackAs(true)
+                   self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+            }
+        }
+
+    }
+
+    @objc(removecache:)
+    func removecache(command:CDVInvokedUrlCommand){
+        filecache.pruneCache()
+        let pluginResult = CDVPluginResult (status: CDVCommandStatus_OK, messageAs: "ok")
+        pluginResult?.setKeepCallbackAs(true)
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+    }
+
+    @objc(cachesize:)
+    func cachesize(command:CDVInvokedUrlCommand){
+        let size = filecache.calculateFolderCacheSize()
+        let pluginResult = CDVPluginResult (status: CDVCommandStatus_OK, messageAs: size)
+        pluginResult?.setKeepCallbackAs(true)
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+    }
+
+    @objc(checkcache:)
+    func checkcache(command:CDVInvokedUrlCommand){
+        let arg = command.arguments[0] as! String
+        let url = URL(string: arg)!
+        if filecache.completeFileExists(remoteFileURL: url) {
+            //如果存在返回local url
+            print("cache exists")
+            let localPath = filecache.localURLFromRemoteURL(url)
+            let pluginResult = CDVPluginResult (status: CDVCommandStatus_OK, messageAs: localPath.absoluteString)
+            pluginResult?.setKeepCallbackAs(true)
+            self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+        }else{
+            print("cache not exists")
+            let _ = filecache.downloadFile(url)
+            let pluginResult = CDVPluginResult (status: CDVCommandStatus_OK, messageAs: url.absoluteString)
+            pluginResult?.setKeepCallbackAs(true)
+            self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+        }
     }
 }
+
--- www/CDVHTTPClient.js
+++ www/CDVHTTPClient.js
@@ -1,27 +1,19 @@
 var exec = require('cordova/exec');
 
-var PLUGIN_NAME = "CDVYPImagePicker"; // This is just for code completion uses.
+var PLUGIN_NAME = "CDVHTTPClient"; // This is just for code completion uses.
 
 var CDVHTTPClient = function() {}; // This just makes it easier for us to export all of the functions at once.
-/*
-options:
- type: photo , video , avatar
- size: Integer , max photo width or height
- filter: Bool , enable/disable show filter
 
-return:
-  photo / avatar:
-    url:   //tmpDirectory file with jpg file type
-    width: //photo's width
-    height: //photo's height
-    size: //file size
-
-  video:
-    url: //tmpDirectory file with mp4 file type
-    duration: //video duration
-    size: //file size
-*/
-CDVHTTPClient.open = function(onSuccess, onError , options) {
-    exec(onSuccess, onError, PLUGIN_NAME, "open", [options]);
+CDVHTTPClient.request = function(onSuccess, onError , options) {
+    exec(onSuccess, onError, PLUGIN_NAME, "request", [options]);
 };
+CDVHTTPClient.checkcache = function(onSuccess,onError,url){
+    exec(onSuccess,onError,PLUGIN_NAME,"checkcache",[url])
+}
+CDVHTTPClient.removecache = function(onSuccess,onError){
+    exec(onSuccess,onError,PLUGIN_NAME,"removecache",[])
+}
+CDVHTTPClient.cachesize = function(onSuccess,onError){
+    exec(onSuccess,onError,PLUGIN_NAME,"cachesize",[])
+}
 module.exports = CDVHTTPClient;
