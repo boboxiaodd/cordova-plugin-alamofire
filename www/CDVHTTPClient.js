c2687c10496cb64692fd35a3c36bd06e3d0da906
bobo <iam386@gmail.com>
2019-11-20 17:18:58 +0800

    add IPA

--- www/CDVHTTPClient.js
+++ www/CDVHTTPClient.js
@@ -1,25 +1,27 @@
-var exec = require('cordova/exec');
+cordova.define("cordova-plugin-http.CDVHTTPClient", function(require, exports, module) {
+    var exec = require('cordova/exec');
 
-var PLUGIN_NAME = "CDVHTTPClient"; // This is just for code completion uses.
+    var PLUGIN_NAME = "CDVHTTPClient"; // This is just for code completion uses.
 
-var CDVHTTPClient = function() {}; // This just makes it easier for us to export all of the functions at once.
+    var CDVHTTPClient = function() {}; // This just makes it easier for us to export all of the functions at once.
 
-CDVHTTPClient.request = function(onSuccess, onError , options) {
-    exec(onSuccess, onError, PLUGIN_NAME, "request", [options]);
-};
-CDVHTTPClient.upload = function(onSuccess, onError , options) {
-    exec(onSuccess, onError, PLUGIN_NAME, "upload", [options]);
-};
-CDVHTTPClient.checkcache = function(onSuccess,onError,url){
-    exec(onSuccess,onError,PLUGIN_NAME,"checkcache",[url])
-};
-CDVHTTPClient.removecache = function(onSuccess,onError){
-    exec(onSuccess,onError,PLUGIN_NAME,"removecache",[])
-};
-CDVHTTPClient.cachesize = function(onSuccess,onError){
-    exec(onSuccess,onError,PLUGIN_NAME,"cachesize",[])
-};
-CDVHTTPClient.purchase = function(onSuccess,onError,param){
-    exec(onSuccess,onError,PLUGIN_NAME,"purchase",[param]);
-};
-module.exports = CDVHTTPClient;
+    CDVHTTPClient.request = function(onSuccess, onError , options) {
+        exec(onSuccess, onError, PLUGIN_NAME, "request", [options]);
+    };
+    CDVHTTPClient.upload = function(onSuccess, onError , options) {
+        exec(onSuccess, onError, PLUGIN_NAME, "upload", [options]);
+    };
+    CDVHTTPClient.checkcache = function(onSuccess,onError,url){
+        exec(onSuccess,onError,PLUGIN_NAME,"checkcache",[url])
+    }
+    CDVHTTPClient.removecache = function(onSuccess,onError){
+        exec(onSuccess,onError,PLUGIN_NAME,"removecache",[])
+    }
+    CDVHTTPClient.cachesize = function(onSuccess,onError){
+        exec(onSuccess,onError,PLUGIN_NAME,"cachesize",[])
+    }
+    CDVHTTPClient.purchase = function(onSuccess,onError,param){
+        exec(onSuccess,onError,PLUGIN_NAME,"purchase",[param]);
+    }
+    module.exports = CDVHTTPClient;
+});
